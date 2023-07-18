16932e23b5a5c963c9e01a3cbd2a3877001a3d27
Maggiflo98 <maggiflo98@gmail.com>
2023-07-18 05:41:36 +0300

    project initialization api setup

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app=express();
+app.use(express.json());
+app.use(cors({origin:true}));
+
+app.post("/authenticate",async(req,res)=>{
+    const {username}=req.body;
+    try{
+        const r=await axios.put(
+            "https://api.chatengine.io/users/",
+            {username:username,secret:username,first_name:username},
+            {headers:{privateKey:"64924c09-f158-4b86-ba5c-6d8e6dec9c62 "}}
+        )
+       return res.status(r.status).json(r.data);
+    }
+    catch(e){
+
+    }
+    return res.json({username:username,secret:"sha256..."});
+});
+
+app.listen(3001);
